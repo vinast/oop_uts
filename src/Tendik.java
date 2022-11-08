42268b8f0943de0cf7b87f28758f92e794f8d8f4
Didik Vinastu <59833466+vinast@users.noreply.github.com>
2022-11-08 11:33:57 +0700

    Update Tendik.java

--- src/Tendik.java
+++ src/Tendik.java
@@ -1,6 +1,6 @@
 public class Tendik extends Karyawan{
     protected int total;
-//    protected int totalGaji;
+
     @Override
     public int gajiPokok() {
         return super.gajiPokok();
@@ -11,8 +11,6 @@ public class Tendik extends Karyawan{
     }
 
     public int lembur(int jam){
-//        gajiPokok();
-//        int total;
          total = 50000 * jam;
         return  total;
     }
